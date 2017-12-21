26096c4cf47493eccab528ac721bcdad0405fdbd
Kyle Stapp <kyle.stapp@starlab.io>
2017-12-21 10:04:09 -0500

    Depriviledged sshd.  Created new user spcifically for virtue app and depriviledged sshd.

--- virtue/kickoff.sh
+++ virtue/kickoff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/root/set-authorized-keys.sh
-xpra start :100 --start-child=gedit
-/usr/sbin/sshd -d
+/home/virtue/set-authorized-keys.sh
+xpra -d all,-window start :100 --start-child=gedit
+/usr/sbin/sshd -d -f /home/virtue/sshd_config
