3ffc70652c709cbb8fc35cee5c9f52aac4ec009f
Kyle Stapp <kyle.stapp@starlab.io>
2018-01-03 14:48:56 -0500

    Added html gedit and firefox hosting

--- virtue/kickoff.sh
+++ virtue/kickoff.sh
@@ -2,4 +2,6 @@
 ~/set-authorized-keys.sh
 mkdir ~/.xpra
 xpra -d all,-window start :100 --start-child=gedit
+xpra start --bind-tcp=0.0.0.0:2023 --html=on --start-child=gedit
+xpra start --bind-tcp=0.0.0.0:2024 --html=on --start-child=firefox
 /usr/sbin/sshd -D -f ~/sshd_config
