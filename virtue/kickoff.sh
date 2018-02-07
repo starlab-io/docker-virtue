f3f2f4359200eff1136769771026c1bf0c31aadf
Kyle Stapp <kyle.stapp@starlab.io>
2018-02-07 00:03:09 -0500

    Updates to Dockerfiles to get common base image, and app specific images, troubleshot crash in firefox and root caused to be shared-memory shortage; adjusted compose.yaml file to account for it.

--- virtue/kickoff.sh
+++ virtue/kickoff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
+export XDG_RUNTIME_DIR=~/.xpra
 ~/set-authorized-keys.sh
 mkdir ~/.xpra
-xpra -d all,-window start :100 --start-child=gedit
-xpra start --bind-tcp=0.0.0.0:2023 --html=on --start-child=gedit
-xpra start --bind-tcp=0.0.0.0:2024 --html=on --start-child=firefox
+xpra start --bind-tcp=0.0.0.0:2023 --html=on --start-child=$APP_TO_RUN
 /usr/sbin/sshd -D -f ~/sshd_config
