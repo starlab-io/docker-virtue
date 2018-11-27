151788cc9cd7ee3a8141a5fe586b92ea00c78d5f
jprice <jprice@bbn.com>
2018-11-27 11:14:03 -0500

    Update path for chmod of init script

--- elastic/image_pull.sh
+++ elastic/image_pull.sh
@@ -3,4 +3,4 @@ sudo usermod -aG docker your-user
 docker pull docker.elastic.co/elasticsearch/elasticsearch:5.6.3
 docker pull docker.elastic.co/kibana/kibana:5.6.3
 
-chmod +x esearch/searchguard/bin/init_sg.sh
+chmod +x ~/docker-virtue/elastic/esearch/searchguard/bin/init_sg.sh
