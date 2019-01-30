195ff8ea9c126f98169201f99ed606eb8fcd254e
Zachary Ratliff <zachary.b.ratliff@raytheon.com>
2019-01-30 20:49:48 +0000

    Added preliminary changes for implementing iptable rules inside individual docker containers

---
+++ virtue/virtue-base/kickoff_root.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is ran by the root user; add any necessary
+# root commands that must be ran during startup such as
+# adding iptable rules.
+
+
+# Loop through the iptable.rules file and add them to the
+# iptable rule chains
+cat /root/iptables.rules | while read line; do
+	iptables $line
+done
+
+
+# Since the virtue user has a `nologin` shell, we cannot simply
+# `su` to virtue. Instead we execute the script on behalf of the
+# virtue user.
+su -s /bin/sh -c '/home/virtue/kickoff.sh' virtue
