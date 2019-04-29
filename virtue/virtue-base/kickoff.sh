85204bda2d596b9888b6a671ac9063669c3ba13d
Jeffrey Mitchell <jeffrey.mitchell@starlab.io>
2019-04-29 10:13:16 -0500

    Removed line which would rewrite the container's ~/.ssh/authorized_keys. This is because Excalibur now does that job with user-specific keys, so users cannot access virtues they are not authorized for.

--- virtue/virtue-base/kickoff.sh
+++ virtue/virtue-base/kickoff.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export XDG_RUNTIME_DIR=~/.xpra
-~/set-authorized-keys.sh
 mkdir ~/.xpra
 xpra start --bind-ws=0.0.0.0:2023 --start-child="$APP_TO_RUN" --log-file=/home/virtue/xpra.log --exit-with-children
 /usr/sbin/sshd -D -f ~/sshd_config
